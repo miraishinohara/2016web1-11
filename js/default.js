ecd02bf09bd33f612a33ab68c68320ab08979820
miraishinohara <miraiminato8@yahoo.co.jp>
2016-06-20 17:47:47 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,5 @@
 var number = Math.floor(Math.random()*3);
-var answer = perseInt(window.prompt('数あてゲーム。0~2の数字を入力してください！'));
+var answer = parseInt(window.prompt('数あてゲーム。0~2の数字を入力してください！'));
 
 var message;
 if(answer === number) {
